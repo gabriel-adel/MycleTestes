64e59897446ffd591223e230bca308e285feaffc
gabriel-adel <36389096+gabriel-adel@users.noreply.github.com>
2020-04-20 01:57:56 -0300

    Update Mycle08-04-2019.sql

--- bla/sql/Mycle08-04-2019.sql
+++ bla/sql/Mycle08-04-2019.sql
@@ -1,26 +1,28 @@
 
-
-create table produto(idProduto int not null auto_increment primary key, nomeProduto varchar(40) not null, cor varchar(20),valorAtacad decimal(5,2),valorVarejo decimal(5,2) not null, descProduto
+create table produto(id int not null auto_increment primary key, nomeProduto varchar(100) not null, cor varchar(20),valorAtacad decimal(5,2),valorVarejo decimal(5,2) not null, descProduto
 	varchar(200) not null, codigoProduto varchar(32) not null, dataProduto date not null, tamanhoMinimo int, tamanhoMaximo int,tag varchar(200))Engine = Innodb, charset = utf8;
 
-create table conta(idLogin int not null primary key auto_increment, usuario varchar(100) not null, senha varchar(32) not null, id_pessoa int not null)Engine = Innodb, charset  = utf8;
+create table conta(id int not null primary key auto_increment, usuario varchar(100) not null, senha varchar(32) not null)Engine = Innodb, charset  = utf8;
 
-create table contato(idContato int not null primary key auto_increment, email varchar(255) not null, telefone varchar(16), celular varchar(16) not null,id_pessoa int not null)Engine = Innodb, charset  = utf8;
+create table contato(id int not null primary key auto_increment, email varchar(255) not null, telefone varchar(16), celular varchar(16) not null,id_pessoa int not null)Engine = Innodb, charset  = utf8;
 
-create table endereco(idEndereco int not null primary key auto_increment, estado varchar(50) not null, cidade varchar(50) not null,bairro varchar(50) not null 
+create table endereco(id int not null primary key auto_increment, estado varchar(50) not null, cidade varchar(50) not null,bairro varchar(50) not null 
 	,rua varchar(50) not null,numero int not null, cep varchar(10),id_pessoa int not null,complemento varchar(100))Engine = Innodb, charset  = utf8;
 
-create table pessoa(idPessoa int not null primary key auto_increment,nome varchar(200) not null, dataCadastro date not null, sexo enum('F','M'), id_login int not null,id_dados_pessoais int not null)Engine = Innodb, charset  = utf8;
+create table pessoa(id int not null primary key auto_increment,nome varchar(200) not null, sexo boolean, id_login int not null,id_dados_pessoais int not null)Engine = Innodb, charset  = utf8;
 
-create table dadosPessoais(idDadosPessoais int not null auto_increment primary key,cpf varchar(15) not null,dataNascimento date not null)Engine = Innodb, charset  = utf8;
+create table dadosPessoais(id int not null auto_increment primary key,cpf varchar(15) not null,dataNascimento date not null)Engine = Innodb, charset  = utf8;
 
-create table pedidos(idPedido int not null primary key auto_increment, id_produto int not null, valorTotal decimal(6,2) not null, quantidade int not null,  id_pessoa int not null,dataCompra date)Engine = Innodb,charset = utf8;
+create table pedidos(id int not null primary key auto_increment, id_produto int not null, valorTotal decimal(6,2) not null, quantidade int not null,  id_pessoa int not null,dataCompra date)Engine = Innodb,charset = utf8;
 
+alter table contato add foreign key(id_pessoa) references pessoa(id);
 
+alter table endereco add foreign key(id_pessoa) references pessoa(id);
 
-alter table contato add foreign key(id_pessoa) references pessoa(idPessoa);
-alter table endereco add foreign key(id_pessoa) references pessoa(idPessoa);
-alter table pessoa add foreign key(id_login) references conta(idLogin);
-alter table pessoa add foreign key(id_dados_pessoais) references pessoa(idPessoa);
-alter table pedidos add foreign key(id_produto) references produto(idProduto);
-alter table pedidos add foreign key(id_pessoa) references pessoa(idPessoa);
+alter table pessoa add foreign key(id_login) references conta(id);
+
+alter table pessoa add foreign key(id_dados_pessoais) references pessoa(id);
+
+alter table pedidos add foreign key(id_produto) references produto(id);
+
+alter table pedidos add foreign key(id_pessoa) references pessoa(id);
