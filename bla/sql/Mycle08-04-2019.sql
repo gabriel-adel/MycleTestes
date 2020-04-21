7a38ffcbf38545f216f49ae082e63ac130823c1b
gabriel-adel <36389096+gabriel-adel@users.noreply.github.com>
2020-04-21 16:53:36 -0300

    Update Mycle08-04-2019.sql

--- bla/sql/Mycle08-04-2019.sql
+++ bla/sql/Mycle08-04-2019.sql
@@ -1,15 +1,15 @@
 
-create table produto(id int not null auto_increment primary key, nomeProduto varchar(100) not null, cor varchar(20),valorAtacad decimal(5,2),valorVarejo decimal(5,2) not null, descProduto
+create table produtos(id int not null auto_increment primary key, nomeProduto varchar(100) not null, cor varchar(20),valorAtacad decimal(5,2),valorVarejo decimal(5,2) not null, descProduto
 	varchar(200) not null, codigoProduto varchar(32) not null, dataProduto date not null, tamanhoMinimo int, tamanhoMaximo int,tag varchar(200))Engine = Innodb, charset = utf8;
 
-create table conta(id int not null primary key auto_increment, usuario varchar(100) not null, senha varchar(32) not null)Engine = Innodb, charset  = utf8;
+create table contas(id int not null primary key auto_increment, usuario varchar(100) not null, senha varchar(32) not null)Engine = Innodb, charset  = utf8;
 
-create table contato(id int not null primary key auto_increment, email varchar(255) not null, telefone varchar(16), celular varchar(16) not null,id_pessoa int not null)Engine = Innodb, charset  = utf8;
+create table contatos(id int not null primary key auto_increment, email varchar(255) not null, telefone varchar(16), celular varchar(16) not null,id_pessoa int not null)Engine = Innodb, charset  = utf8;
 
-create table endereco(id int not null primary key auto_increment, estado varchar(50) not null, cidade varchar(50) not null,bairro varchar(50) not null 
+create table enderecos(id int not null primary key auto_increment, estado varchar(50) not null, cidade varchar(50) not null,bairro varchar(50) not null 
 	,rua varchar(50) not null,numero int not null, cep varchar(10),id_pessoa int not null,complemento varchar(100))Engine = Innodb, charset  = utf8;
 
-create table pessoa(id int not null primary key auto_increment,nome varchar(200) not null, sexo boolean, id_login int not null,id_dados_pessoais int not null)Engine = Innodb, charset  = utf8;
+create table pessoas(id int not null primary key auto_increment,nome varchar(200) not null, sexo boolean, id_login int not null,id_dados_pessoais int not null)Engine = Innodb, charset  = utf8;
 
 create table dadosPessoais(id int not null auto_increment primary key,cpf varchar(15) not null,dataNascimento date not null)Engine = Innodb, charset  = utf8;
 
